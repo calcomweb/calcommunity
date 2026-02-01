a9c922d1739b56ea4481780dbd8d21372edb2bdd
ubterzioglu <ubterzioglu@gmail.com>
2026-02-01 14:51:02 +0100

    Remove Lovable refs, add Clarity, update favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import Clarity from "@microsoft/clarity";
+
+const clarityId = import.meta.env.VITE_CLARITY_ID;
+if (clarityId) {
+  Clarity.init(clarityId);
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
